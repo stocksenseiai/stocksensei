96a7d4711db10e7512707d82bf9dca012bc49b66
stocksenseiai <detjetme@gmail.com>
2025-11-13 03:24:10 -0500

    Add files via upload

---
+++ deepseek_javascript_20251113_654e75.js
@@ -0,0 +1,18 @@
+// StockSensei JavaScript
+function showNotification(message, type = 'info') {
+    // Notification code from earlier
+}
+
+function showLoginModal() {
+    // Login modal code from earlier
+}
+
+function signupUser(tier) {
+    // Signup code from earlier  
+}
+
+function analyzeStock() {
+    // Stock analysis code from earlier
+}
+
+// All other JavaScript functions from earlier
